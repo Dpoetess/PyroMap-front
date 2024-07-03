a7dfc20fc1ad406c6a81e5667a7b86d5b4bedc8a
Lynn Poh <pohwlynn@gmail.com>
2024-07-03 23:38:13 +0200

    installed Axios + fetched API data

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,7 @@
+/* import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
 
 function App() {
   
@@ -10,3 +14,26 @@ function App() {
 }
 
 export default App
+ */
+
+
+import React from 'react';
+import useAPI from './services/UseApi';
+import { worldArea_url } from './config/urls';
+
+
+const App = () => {
+  const { data, loading, error } = useAPI(worldArea_url);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  return (
+    <div>
+      <h1>Data from API</h1>
+      <pre>{data}</pre>
+    </div>
+  );
+};
+
+export default App;
