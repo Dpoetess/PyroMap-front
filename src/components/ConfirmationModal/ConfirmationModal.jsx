fcd5075d398ea1409082b00cd0f4f468cc1755ef
[Michely Paredes Galeano] <[michelyparedes@gmail.com]>
2024-07-05 00:26:52 +0200

    change text on confirmarion modal

--- src/components/ConfirmationModal/ConfirmationModal.jsx
+++ src/components/ConfirmationModal/ConfirmationModal.jsx
@@ -1,22 +1,26 @@
 /* eslint-disable react/prop-types */
 
-import Button from '../Button/Button';
-import Modal from 'react-bootstrap/Modal';
+import Button from "../Button/Button";
+import Modal from "react-bootstrap/Modal";
 
-function ConfirmationModal({show, handleClose}) {
-
-    return (
-            <Modal show={show} onHide={handleClose}>
-                <Modal.Header closeButton>
-                    <Modal.Title>Modal heading</Modal.Title>
-                </Modal.Header>
-                <Modal.Body>Woohoo, you are reading this text in a modal!</Modal.Body>
-                <Modal.Footer>
-                    <Button handleClick={() => {handleClose()}} text={"Close"} type="button"/>
-                </Modal.Footer>
-            </Modal>
-    );
+function ConfirmationModal({ show, handleClose }) {
+  return (
+    <Modal show={show} onHide={handleClose}>
+      <Modal.Header closeButton>
+        <Modal.Title>¡Muchas gracias!</Modal.Title>
+      </Modal.Header>
+      <Modal.Body>Inscripción realizada correctamente.</Modal.Body>
+      <Modal.Footer>
+        <Button
+          handleClick={() => {
+            handleClose();
+          }}
+          text={"Close"}
+          type="button"
+        />
+      </Modal.Footer>
+    </Modal>
+  );
 }
 
 export default ConfirmationModal;
-
