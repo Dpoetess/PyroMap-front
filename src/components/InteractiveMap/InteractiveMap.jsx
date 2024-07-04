78f685fc4a10061ad3222fed80564a8b819699ef
Valentina Toni <vale.toni93@gmail.com>
2024-07-04 10:39:18 +0200

    done filter functionality

--- src/components/InteractiveMap/InteractiveMap.jsx
+++ src/components/InteractiveMap/InteractiveMap.jsx
@@ -1,47 +1,75 @@
-import React from 'react';
-import { useState } from 'react';
-import { APIProvider, Map, AdvancedMarker, InfoWindow, Pin } from '@vis.gl/react-google-maps';
-import { Flame } from 'lucide-react';
+import React, { useState, useEffect } from 'react';
+import { APIProvider, Map, AdvancedMarker, InfoWindow } from '@vis.gl/react-google-maps';
+import { Flame, MapPin } from 'lucide-react';
 import { fireData } from '../../data/fakeDataToTest';
 import './InteractiveMap.scss';
+import SearchInput from '../SearchInput/SearchInput';
 
 const InteractiveMap = () => {
-    const dataToDisplay = fireData.map((fire, index) => ({
-        key: fire.acq_date + index, 
-        location: {
-            lat: fire.latitude,
-            lng: fire.longitude,
-        },
-        info: `Fire detected on ${fire.acq_date} at ${fire.acq_time}`
-    }));
+    const [userLocation, setUserLocation] = useState(null);
+    const [selectedMarker, setSelectedMarker] = useState(null);
+    const [mapCenter, setMapCenter] = useState({ lat: 22.54992, lng: 0 });
+    const [mapZoom, setMapZoom] = useState(2);
 
     const apiKey = import.meta.env.VITE_API_KEY;
     const mapID = import.meta.env.VITE_MAP_ID;
 
-    const [selectedMarker, setSelectedMarker] = useState(null);
+    const handleSearch = (location) => {
+        setUserLocation(location);
+        setMapCenter(location);
+        setMapZoom(5); 
+    };
+
+    const getDistance = (lat1, lon1, lat2, lon2) => {
+        const R = 6371; // Radius of the Earth in km
+        const dLat = (lat2 - lat1) * Math.PI / 180;
+        const dLon = (lon2 - lon1) * Math.PI / 180;
+        const a =
+            0.5 - Math.cos(dLat) / 2 +
+            Math.cos(lat1 * Math.PI / 180) * Math.cos(lat2 * Math.PI / 180) *
+            (1 - Math.cos(dLon)) / 2;
+
+        return R * 2 * Math.asin(Math.sqrt(a));
+    };
+
+    const filteredData = userLocation ? fireData.filter(fire => {
+        const distance = getDistance(userLocation.lat, userLocation.lng, fire.latitude, fire.longitude);
+        return distance <= 200; // Filtra los incendios a 50 km de la ubicación del usuario
+    }) : fireData;
+
+    console.log(filteredData);
+    
 
     return (
         <APIProvider apiKey={apiKey}>
-            <div className="map" style={{height: "60vh", width: "80vw"}}>
+            <SearchInput onSearch={handleSearch} />
+            <h1 className=" u-text-accent u-font-bold u-font-xlarge">Incendios activos actualmente</h1>
+            <div className="map">
                 <Map
-                    defaultCenter={{ lat: 22.54992, lng: 0 }}
-                    defaultZoom={3}
+                    defaultCenter={mapCenter}
+                    deafultZoom={mapZoom}
                     mapId={mapID}
                 >
-                    {dataToDisplay.map((fire, index) => (
-                        <React.Fragment key={fire.key}>
+                    {/* {userLocation && (
+                        <AdvancedMarker
+                            position={userLocation}
+                            icon={{ path: MapPin, scale: 1, strokeColor: '#e41111', strokeWeight: 2 }}
+                        />
+                    )} */}
+                    {filteredData.map((fire, index) => (
+                        <React.Fragment key={index}>
                             <AdvancedMarker
-                                position={fire.location}
+                                position={{ lat: fire.latitude, lng: fire.longitude }}
                                 onClick={() => setSelectedMarker(index)}
                             >
                                 <Flame size={16} color="#e41111" strokeWidth={3} absoluteStrokeWidth />
                             </AdvancedMarker>
                             {selectedMarker === index && (
                                 <InfoWindow
-                                    position={fire.location}
+                                    position={{ lat: fire.latitude, lng: fire.longitude }}
                                     onCloseClick={() => setSelectedMarker(null)}
                                 >
-                                    <p>{fire.info}</p>
+                                    <p>Fire detected on {fire.acq_date} at {fire.acq_time}</p>
                                 </InfoWindow>
                             )}
                         </React.Fragment>
@@ -53,3 +81,4 @@ const InteractiveMap = () => {
 };
 
 export default InteractiveMap;
+
