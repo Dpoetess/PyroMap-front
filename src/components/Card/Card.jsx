ec73dbc7178f40101cb6561dd5e39f12e4d1d5b2
tursdlc <ana89.sdlc@gmail.com>
2024-07-04 09:49:08 +0200

    accepting changes develop

---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,19 @@
+/* eslint-disable react/prop-types */
+import '../../styles/index.scss'
+import './Card.scss'
+
+
+
+const Card = ({title, description}) => {
+    return (
+        <div className="card">
+            <header>
+                <h2 className="u-font-bold u-font-large">{title}</h2>
+            </header>
+            <main>
+                <p>{description}</p>
+            </main>
+        </div>
+    )
+}
+export default Card
