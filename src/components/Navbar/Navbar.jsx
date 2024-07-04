c482f9d3f278a6a3ba7f1d58629fc00519b2959a
tursdlc <ana89.sdlc@gmail.com>
2024-07-04 12:16:54 +0200

    create navbar and links

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,18 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+import './Navbar.scss'
+
 const Navbar = () => {
-    return (
-        <div>Ejemplo Navbar</div>
-    )
+    return (    
+        <div className='navBar'>
+            <div className='navBarText'>
+                <Link to="Register/Login" className='linkNavbar'>Iniciar sesión</Link>
+                <Link to="/Register" className='linkNavbar'>Registrarse</Link>
+            </div>
+            
+                
+        </div>
+        
+)
 }
 export default Navbar
