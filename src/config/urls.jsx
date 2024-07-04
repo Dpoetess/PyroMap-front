ee99d1313be46a14a88c8e08e215b34c63c2af37
Lynn Poh <pohwlynn@gmail.com>
2024-07-04 12:32:05 +0200

    added Spain endpoint + changed useAPI url to Spain

--- src/config/urls.jsx
+++ src/config/urls.jsx
@@ -1,2 +1,3 @@
 export const base_url="https://firms.modaps.eosdis.nasa.gov/api";
 export const worldArea_url= `${base_url}/area/csv/643987db9b0c8ec0b15db287839ba061/VIIRS_SNPP_NRT/world/1`;
+export const countrySpain_url= `${base_url}/country/csv/643987db9b0c8ec0b15db287839ba061/VIIRS_SNPP_NRT/ESP/1`;
